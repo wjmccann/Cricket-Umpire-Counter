206e6bf1c55cf88b7b43def36b8f726e0805ce09
wjmccann <wjmccann@live.com.au>
2017-02-20 13:41:27 +1100

    add timer to splash screen

--- app/src/main/java/com/wjmccann/cricketumpirecounter/MainActivity.java
+++ app/src/main/java/com/wjmccann/cricketumpirecounter/MainActivity.java
@@ -1,6 +1,7 @@
 package com.wjmccann.cricketumpirecounter;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+            public void run(){
+                Intent intent = new Intent(MainActivity.this, Counter.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 
     public void onSplashClick(View v){
